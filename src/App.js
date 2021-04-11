72d6469bc1589417a2a8424ec81ce5c078777833
Tahir Ahmed <53032124+ttahm3d@users.noreply.github.com>
2021-04-11 21:58:15 +0530

    firebase auth added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import TodoList from "./components/TodoList/TodoList";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "./firebase.config";
+import firebase from "firebase";
 import "./App.css";
 
+const sighInWithGoogle = () =>
+  auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+
+const SignIn = () => (
+  <main>
+    <button onClick={sighInWithGoogle}>Sign In With Google</button>
+  </main>
+);
+
 function App() {
-  return (
-    <div className="App">
-      <TodoList />
-    </div>
-  );
+  const [user] = useAuthState(auth);
+
+  return <div className="App">{user ? <TodoList /> : <SignIn />}</div>;
 }
+
+// const SingIn = () => {
+//   return (
+//     <div>
+//       <button onClick={OAuthLoginWithGoogle}> SignIn</button>
+//     </div>
+//   );
+// };
 
 export default App;
--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,15 +1,30 @@
 import React, { useState } from "react";
 import Todo from "../Todo/Todo";
+import firebase from "firebase";
+import { useCollectionData } from "react-firebase-hooks/firestore";
 import { Header, TodoContainer, FormContainer } from "./TodoList.styles";
 import { AiOutlinePlusCircle } from "react-icons/ai";
+import { auth, firestore } from "../../firebase.config";
 
 const TodoList = () => {
   const [todo, setTodo] = useState("");
+  const todosRef = firestore.collection(`users/${auth.currentUser.uid}/todos`);
+
+  const [todos] = useCollectionData(todosRef);
 
   const handleSubmit = (event) => {
     event.preventDefault();
 
     setTodo("");
+    todosRef.add({
+      text: todo,
+      complete: false,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+    });
+  };
+
+  const signOut = () => {
+    auth.signOut();
   };
 
   return (
@@ -19,7 +34,7 @@ const TodoList = () => {
           <h1>
             To<span>do</span>
           </h1>
-          <p>Sign out</p>
+          <p onClick={signOut}>Sign out</p>
         </nav>
       </Header>
       <FormContainer>
